98c3d849be581f379111bbbbe631ba1bfa744de9
grayburnd <grayburnd@yahoo.co.uk>
2026-08-29 12:16:45 +0100

    fix: fixed pre commit so runs on eachcommit

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,42 +1,43 @@
 import os
-from pathlib import Path
-from testcontainers.compose import DockerCompose
-import pytest
-import pytest_mock
-import requests
-import psycopg
 import random
 import time
+from collections.abc import Callable
+from pathlib import Path
+
+import psycopg
+import pytest
+import pytest_mock
+import redis
 import redis.exceptions
 import redis.sentinel
-import redis
-from flask import Flask
-from collections.abc import Callable
-
-from app import (
-    get_redis,
-    main
-)
-
+import requests
 from dotenv import load_dotenv
+from flask import Flask
+from testcontainers.compose import DockerCompose
 
-#Initialize Root path
+from app import get_redis, main
+
+# Initialize Root path
 path = Path(__file__).parent.parent.resolve()
 
-#Initialize env vars into run context
-load_dotenv() 
+# Initialize env vars into run context
+load_dotenv()
 
-#Initialize docker environment - essentially runs and maintains docker compose up.
+
+# Initialize docker environment - essentially runs and maintains docker compose up.
 @pytest.fixture(scope="module")
 def docker_env():
     with DockerCompose(
-        path, compose_file_name=["compose.yml"], pull=False ##Set pull to False to use local images, may need to change in GH Actions
+        path,
+        compose_file_name=["compose.yml"],
+        pull=False,  ##Set pull to False to use local images, may need to change in GH Actions
     ) as compose:
-        compose.start() ##Start env once for duration of tests
+        compose.start()  ##Start env once for duration of tests
         yield compose
         compose.stop()
 
-#Run len(vote_choice) number of tests to test the votes
+
+# Run len(vote_choice) number of tests to test the votes
 @pytest.mark.parametrize("vote_choice", [("a"), ("b")])
 def test_vote_postgres_count(docker_env: DockerCompose, vote_choice: str):
     stdout, stderr = docker_env.get_logs()
@@ -57,60 +58,85 @@ def test_vote_postgres_count(docker_env: DockerCompose, vote_choice: str):
                 f"Attempt {attempt}, sending vote with voter_id {voter_id}..."
             )
             response = requests.post(url, data=vote_data, cookies=cookies)
-            response.raise_for_status
+            response.raise_for_status()
             attempt += 1
             time.sleep(3)
     except requests.exceptions.HTTPError as e:
-        raise requests.exceptions.HTTPError(f"failed with error code: {e.errno}\nerror: {e}")
-    postgres_hostname = docker_env.get_service_host(os.environ["DB_HOST"],int(os.environ["DB_PORT"])) ##Use the docker service name and resolve the endpoint exposed to the host #0.0.0.0:5432 -> container postgres:5432
+        raise requests.exceptions.HTTPError(
+            f"failed with error code: {e.errno}\nerror: {e}"
+        )
+    postgres_hostname = docker_env.get_service_host(
+        os.environ["DB_HOST"], int(os.environ["DB_PORT"])
+    )  ##Use the docker service name and resolve the endpoint exposed to the host #0.0.0.0:5432 -> container postgres:5432
     print(f"postgres_hostname is {postgres_hostname}")
-    with psycopg.connect(
-        f"""dbname={os.environ["DB"]}
+    with (
+        psycopg.connect(f"""dbname={os.environ["DB"]}
     user={os.environ["DB_USERNAME"]}
-    host={postgres_hostname} 
-    password={os.environ["DB_PASSWORD"]}"""
-    ) as postgres_conn:
-        with postgres_conn.cursor() as cur:
-            cur.execute("""
-                SELECT vote, COUNT(*) AS 
-                total FROM votes GROUP BY 
-                vote ORDER BY vote
-            """)
-            result = cur.fetchall()
-            count_of_votes = result[0][1]
-            print(f"attempt = {attempt}, count_of_votes = {count_of_votes}")
-            assert count_of_votes == attempt
+    host={postgres_hostname}
+    password={os.environ["DB_PASSWORD"]}""") as postgres_conn,
+        postgres_conn.cursor() as cur,
+    ):
+        cur.execute("""
+            SELECT vote, COUNT(*) AS
+            total FROM votes GROUP BY
+            vote ORDER BY vote
+        """)
+        result = cur.fetchall()
+        count_of_votes = result[0][1]
+        print(f"attempt = {attempt}, count_of_votes = {count_of_votes}")
+        assert count_of_votes == attempt
 
-@pytest.mark.parametrize("target_method,side_effect,error_raised,target_func,message", 
-                         [("redis.Sentinel",
-                           redis.sentinel.MasterNotFoundError,
-                           redis.RedisError,get_redis,
-                           "Master not Found for redis Cluster"),
-                           ("redis.Sentinel.master_for",
-                            redis.sentinel.MasterNotFoundError,
-                            redis.RedisError,
-                            get_redis,
-                            "Master not Found for redis Cluster")],
-                           ids=["redis.Sentinel","redis.Sentinel.master_for"])
-def test_raises(mocker: pytest_mock.MockerFixture, target_method: str, side_effect: type[Exception], error_raised: type[Exception], target_func: Callable[[], redis.Redis], message: str):
+
+@pytest.mark.parametrize(
+    "target_method,side_effect,error_raised,target_func,message",
+    [
+        (
+            "redis.Sentinel",
+            redis.sentinel.MasterNotFoundError,
+            redis.RedisError,
+            get_redis,
+            "Master not Found for redis Cluster",
+        ),
+        (
+            "redis.Sentinel.master_for",
+            redis.sentinel.MasterNotFoundError,
+            redis.RedisError,
+            get_redis,
+            "Master not Found for redis Cluster",
+        ),
+    ],
+    ids=["redis.Sentinel", "redis.Sentinel.master_for"],
+)
+def test_raises(
+    mocker: pytest_mock.MockerFixture,
+    target_method: str,
+    side_effect: type[Exception],
+    error_raised: type[Exception],
+    target_func: Callable[[], redis.Redis],
+    message: str,
+):
     flask_app = Flask(__name__, root_path=str(path))
     with flask_app.app_context():
-        mocker.patch(
-            target_method, side_effect=side_effect(message)
-        )
+        mocker.patch(target_method, side_effect=side_effect(message))
         with pytest.raises(error_raised):
             target_func()
+
 
 def test_rpush_raises(mocker: pytest_mock.MockerFixture):
     # - Need to see cos the env vars aren't getting used inside the container, its the context of my shell instead i.e REDIS_SENTINEL_HOST
     flask_app = Flask(__name__, root_path=str(path))
-    with flask_app.test_request_context("/vote", method="POST", data={"vote": "a"}):
+    with flask_app.test_request_context(
+        "/vote", method="POST", data={"vote": "a"}
+    ):
         mocker.patch(
-            "redis.Redis.rpush", side_effect=redis.exceptions.RedisError(f"There has been a Redis Error")
+            "redis.Redis.rpush",
+            side_effect=redis.exceptions.RedisError(
+                "There has been a Redis Error"
+            ),
         )
         with pytest.raises(redis.RedisError):
             main()
 
 
-#TODO
+# TODO
 # - Connect to Postgres directly - for rendering results selenium needs to be used
