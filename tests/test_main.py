b25bed0857bd255e6e70231500f305912f2bb54c
grayburnd <grayburnd@yahoo.co.uk>
2026-09-01 16:20:41 +0100

    fix(docker-compose): Debugging docker compose on GH Actions Runner

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,20 +29,14 @@ if not os.getenv("GITHUB_ENV"):
 # Initialize docker environment - essentially runs and maintains docker compose up.
 @pytest.fixture(scope="module")
 def docker_env():
-    try:
-        with DockerCompose(
-            path,
-            compose_file_name=["compose.yml"],
-            pull=True,
-        ) as compose:
-            compose.start()  ##Start env once for duration of tests
-            yield compose
-            compose.stop()
-    except Exception as e:
-        print(
-            f"Got Docker Compose Exception:\nError: {e}"
-        )
-
+    with DockerCompose(
+        path,
+        compose_file_name=["compose.yml"],
+        pull=True,
+    ) as compose:
+        compose.start()  ##Start env once for duration of tests
+        yield compose
+        compose.stop()
 
 # Run len(vote_choice) number of tests to test the votes
 @pytest.mark.parametrize("vote_choice", [("a"), ("b")])
